015aa01fc5712fca3bf063550a2d291282beb720
pedram aghasian <pedram.aghasian@yahoo.com>
2020-02-18 03:10:26 +0800

    back to master

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require_relative 'boot'
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,7 +12,7 @@ module Myrecipes
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
