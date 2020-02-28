46514a2f12f17e7e8f0ff032e933a6fe5d355d9a
pedram aghasian <pedram.aghasian@yahoo.com>
2020-02-28 21:02:15 +0800

    edit and delete

---
+++ test/integration/recipes_delete_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class RecipesDeleteTest < ActionDispatch::IntegrationTest
+
+
+  def setup
+    @chef = Chef.create!(chefname: "pedram", email: "pedram@yahoo.com")
+    @recipe= Recipe.create(name: " dfdfsd", description: "dfsdfd",chef: @chef)
+  end
+
+  test "successfully delete a recipe" do  
+    get recipe_path(@recipe)
+    assert_template 'recipe/show'
+    assert_select 'a[href=?]',recipe_path(@recipe),text: "Delete this recipe"
+    assert_difference 'Recipe.count', -1 do 
+      delete recipe_path(@recipe)
+    end
+    assert_redirect_to recipes_path
+    assert_not flash.empty?
+  end
+end
---
+++ test/integration/recipes_edit_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class RecipesEditTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @chef = Chef.create!(chefname: "pedram", email: "pedram@yahoo.com")
+    @recipe= Recipe.create(name: " dfdfsd", description: "dfsdfd",chef: @chef)
+  end
+  
+  test "reject invalid recipe update" do 
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    patch recipe_path(@recipe),params: {recipe: {name:" ", description: " dfdsfsdfsdfsdf dfsd"}}
+    assert_template 'recipes/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+
+  test "successfully edit a recipe "do  
+    get edit_recipe_path(@recipe)
+    assert_template 'recipes/edit'
+    updated_name= " dkfjsdklfjskdlfjsdklf"
+    updated_description = "dsfdsfsdfsdfdsfds"
+    path recipe_path(@recipe),params: {recipe: {name: updated_name,description: updated_description}}
+    assert_not flash.empty?
+    @recipe.reload
+    assert_match updated_name, @Recipe.name 
+    assert_match updated_description, @Recipe.description
+  end
+
+end
