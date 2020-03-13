acdcc8ceea226bee4b23fc7ce1aa246b0b33b6d8
pedram aghasian <pedram.aghasian@yahoo.com>
2020-03-13 22:59:03 +0800

    add comments model with assoc

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -8,5 +8,7 @@ class Chef < ApplicationRecord
   has_many :recipes , dependent: :destroy  
   has_secure_password 
   validates :password , presence: true , length: {minimum:7} , allow_nil: true    
-  default_scope -> {order(updated_at: :desc)}      
+  default_scope -> {order(updated_at: :desc)}  
+  
+  has_many :comments, dependent: :destroy
 end
